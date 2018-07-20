e6500107d7831b56862ce42af84767433b66ed8c
Felipe Mota <felipe.mota@hp.com>
2018-07-19 22:19:01 -0300

    Opening repo to github

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, Text, View, Slider } from 'react-native'
+import AddEntry from './components/AddEntry'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <AddEntry />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: "center"
+  }
+})
